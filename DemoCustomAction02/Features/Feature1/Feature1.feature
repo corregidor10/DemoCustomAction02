ccd8b8f3aa69546cfbdfd4179cb30d893a591df0
Daniel Corregidor Coronado <daniel.corregidor@tajamar365.com>
2016-04-11 15:58:20 +0000

    2nd commit

--- DemoCustomAction02/Features/Feature1/Feature1.feature
+++ DemoCustomAction02/Features/Feature1/Feature1.feature
@@ -5,5 +5,12 @@
     <projectItemReference itemId="0c6acfd7-bfe7-47cc-bdda-2ff82a620437" />
     <projectItemReference itemId="d9e578dd-6453-496e-b0ed-ec7e24e1f603" />
     <projectItemReference itemId="717c1597-a57c-4cf3-b58f-48077a565364" />
+    <projectItemReference itemId="cab0da8c-ed72-4149-8e1e-aa3f46866610" />
+    <projectItemReference itemId="40b1e8c7-6d69-4094-88a1-6addd110de38" />
+    <projectItemReference itemId="74ff1110-8682-48d3-8a57-5a71b687fc20" />
+    <projectItemReference itemId="0f1168e1-c5b2-48a8-ab79-513d0c3a8f36" />
+    <projectItemReference itemId="c078f836-ffbe-4586-b282-ce00b1e0dc3e" />
+    <projectItemReference itemId="fa27d615-dcdf-484e-8ccf-c8bf97aed95f" />
+    <projectItemReference itemId="0b774cdc-b381-438a-b331-52ddc12d4d2b" />
   </projectItems>
 </feature>
---
+++ DemoCustomAction02/Scripts/customizeview.js
@@ -0,0 +1,58 @@
+﻿window.Cust = window.Cust || {};
+window.Cust.accordionItem = {
+    customeItemHtml: function(cntx) {
+        var accordionItemHtml = "<h3>" + cntx.CurrentItem.Nombre + "</h3>";
+        accordionItemHtml += "<div>" + cntx.CurrentItem.Descripcion + "</div>";
+
+        return accordionItemHtml;
+    }
+};
+
+(function() {
+    var appWebUrl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+    var scriptFolder = appWebUrl + "/SiteAssets/";
+    loadcssfile(scriptFolder + "jqueryui.css", "css");
+
+    var overrideCtx = {};
+    overrideCtx.Templates = {};
+    overrideCtx.Templates.Header = "<div id=\"accordion\">";
+    overrideCtx.Templates.Item = window.Cust.accordionItem.customeItemHtml;
+    overrideCtx.Templates.Footer = "</div>";
+    overrideCtx.BaseViewID = 1;
+    overrideCtx.ListTemplateType = 10000;
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrideCtx);
+
+})();
+
+$(document).ready(function() {
+    $("#accordion").find("#scriptBodyWPQ1").remove();
+
+    $("#accordion").accordion({ heightStyle: "content" });
+
+});
+
+function loadcssfile(filename, filetype) {
+    if (filetype=="css") {
+        var fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+    }
+
+    if (typeof fileref!= "undefined") {
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+}
+
+function getQueryStringParameter(urlParameterKey) {
+    var params = document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i++) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0]==urlParameterKey) {
+            return decodeURIComponent(singleParam[1]);
+        }
+    }
+
+}
